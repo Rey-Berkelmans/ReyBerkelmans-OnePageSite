3aa8ae325e38893505d13c8120c69cc90e422f94
Rey-Berkelmans <143152147+Rey-Berkelmans@users.noreply.github.com>
2023-10-29 12:56:29 +1100

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function carousel() {
 }
 
 window.alert("When a bushfire is seen, make sure to report it and contanct emergency help when required.");
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
